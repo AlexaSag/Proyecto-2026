7c1c156f227a3ce92acb71453aa08c0f0b4ca48c
Alexa <alexa@MacBook-Air-de-Alexa.local>
2026-02-22 23:45:33 -0600

    fix render port

--- app.py
+++ app.py
@@ -409,5 +409,8 @@ convert(1);
 </html>
 """
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
