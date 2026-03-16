f908037e39beca58fb3d8361a6761cb165a72945
Alexa <alexa@MacBook-Air-de-Alexa.local>
2026-03-15 23:32:45 -0600

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Alexa Builder</title>
+</head>
+
 <body>
 
 <h1>Alexa Builder</h1>
@@ -21,3 +35,12 @@ FIN DE NOTA
 </a>
 
 </body>
+</html>
+"""
+
+@app.route("/download")
+def download():
+    return send_file("s.a.brain.py.zip", as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
